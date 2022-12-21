b5b1dc297cbb374acc66246f9de82e7f17c522da
virtualynx <eryandi.angga.pratama@gmail.com>
2022-12-21 19:16:50 +0700

    additional field to view_master_eis re-define view_master_eis_active with simpler query

--- Queries-Pre/04_views/view_master_eis.sql
+++ Queries-Pre/04_views/view_master_eis.sql
@@ -9,7 +9,18 @@ CREATE MATERIALIZED VIEW view_master_eis AS
 	    "management_level",
 	    position_id,
 	    department_id,
-	    phone
+	    phone,
+	    npwp,
+	    birth_date,
+	    birth_place,
+		ktp_number,
+		ktp_address,
+		address,
+		last_education,
+		bankaccount_number,
+		bankaccount_name,
+		phk_date,
+		phk_code
 	FROM dblink(
 		'dbname=integration port=5432 host=172.16.123.121 user=postgres password=initrans7'::text, 
 		'
@@ -21,7 +32,18 @@ CREATE MATERIALIZED VIEW view_master_eis AS
 				"ManajemenLevel" as management_level,
 				"KodePosisi" as position_id,
 				"department_code",
-				"NomorHP"
+				"NomorHP",
+				"NPWP",
+				"TanggalLahir",
+				"TempatLahir",
+				"NoKTP",
+				"AlamatKTP",
+				"Alamat",
+				"Pendidikan",
+				"rekening_no",
+				"rekening_nama",
+				"TanggalPHK",
+				"KodePHK"
 			FROM 
 				hris."MasterEis"'::text
 	) tbl (
@@ -32,6 +54,17 @@ CREATE MATERIALIZED VIEW view_master_eis AS
 		"management_level" character varying(30),
 		position_id character varying(50),
 		department_id character varying(50),
-		phone character varying(25)
+		phone character varying(25),
+		npwp character varying(50),
+		birth_date date,
+		birth_place character varying(25),
+		ktp_number character varying(40),
+		ktp_address character varying(100),
+		address character varying(100),
+		last_education character varying(100),
+		bankaccount_number character varying(50),
+		bankaccount_name character varying(50),
+		phk_date date,
+		phk_code int
 	)
 WITH DATA;
