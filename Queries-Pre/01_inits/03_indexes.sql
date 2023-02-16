09317b33651efbd4fa1f0447c2119307287af0a7
virtualynx <eryandi.angga.pratama@gmail.com>
2023-02-16 15:41:14 +0700

    add index for lowercase(name) on master_account

--- Queries-Pre/01_inits/03_indexes.sql
+++ Queries-Pre/01_inits/03_indexes.sql
@@ -1,4 +1,5 @@
 CREATE INDEX IF NOT EXISTS master_account_name_idx ON master_account USING btree (name);
+CREATE INDEX IF NOT EXISTS master_account_name_lowercase_idx ON master_account (lower(name));
 
 CREATE INDEX IF NOT EXISTS transaction_journal_bookdate_idx ON transaction_journal USING btree (bookdate DESC NULLS LAST);
 CREATE INDEX IF NOT EXISTS transaction_journal_created_date_idx ON transaction_journal USING btree (created_date DESC NULLS LAST);
