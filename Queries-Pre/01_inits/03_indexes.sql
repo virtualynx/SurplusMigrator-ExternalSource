0ddf2473a787b1fa2b5afa8aba3862b7d220f394
virtualynx <eryandi.angga.pratama@gmail.com>
2023-02-16 09:33:05 +0700

    add index for column ref_detail_id, ref_id, subreference_id, and ref_subdetail_id in transaction_journal_detail

--- Queries-Pre/01_inits/03_indexes.sql
+++ Queries-Pre/01_inits/03_indexes.sql
@@ -1,3 +1,9 @@
-CREATE INDEX IF NOT EXISTS transaction_journal_bookdate_idx ON transaction_journal USING btree (bookdate);
 CREATE INDEX IF NOT EXISTS master_account_name_idx ON master_account USING btree (name);
-CREATE INDEX IF NOT EXISTS transaction_journal_created_date ON transaction_journal USING btree (created_date DESC NULLS LAST);
+
+CREATE INDEX IF NOT EXISTS transaction_journal_bookdate_idx ON transaction_journal USING btree (bookdate DESC NULLS LAST);
+CREATE INDEX IF NOT EXISTS transaction_journal_created_date_idx ON transaction_journal USING btree (created_date DESC NULLS LAST);
+
+CREATE INDEX IF NOT EXISTS transaction_journal_detail_ref_detail_id_idx ON transaction_journal_detail USING btree (ref_detail_id);
+CREATE INDEX IF NOT EXISTS transaction_journal_detail_ref_id_idx ON transaction_journal_detail USING btree (ref_id);
+CREATE INDEX IF NOT EXISTS transaction_journal_detail_subreference_id_idx ON transaction_journal_detail USING btree (subreference_id);
+CREATE INDEX IF NOT EXISTS transaction_journal_detail_ref_subdetail_id_idx ON transaction_journal_detail USING btree (ref_subdetail_id);
